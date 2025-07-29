166127e73aa7be0aba194e93631409f6452f1dad
Sarah Villarreal Carballo <sarahvc007@gmail.com>
2025-07-29 00:49:25 -0300

    Actualización

---
+++ core/src/main/java/com/punchline/hitlist/pantallaJuego/MapaDisponible.java
@@ -0,0 +1,21 @@
+package com.punchline.hitlist.pantallaJuego;
+
+public enum MapaDisponible {
+    MAPA_BOSQUE("mapas/mapa.tmx", "fondos/fondo.png");
+
+    private final String mapaTmx;
+    private final String fondoPng;
+
+    MapaDisponible(String mapaTmx, String fondoPng) {
+        this.mapaTmx = mapaTmx;
+        this.fondoPng = fondoPng;
+    }
+
+    public String getmapaTmx() {
+        return mapaTmx;
+    }
+
+    public String getfondoPng() {
+        return fondoPng;
+    }
+}
