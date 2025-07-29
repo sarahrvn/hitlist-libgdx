840336deb83f30c062020b93944db4f3f81c1de6
Thiago Ivan Orue Stocco <thiagoivanorue@gmail.com>
2025-07-29 00:02:13 -0300

    Actualizacion

--- core/src/main/java/com/punchline/hitlist/Principal.java
+++ core/src/main/java/com/punchline/hitlist/Principal.java
@@ -4,60 +4,23 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.punchline.hitlist.pantallaJuego.Mapa;
+import com.punchline.hitlist.pantallaJuego.MapasDisponibles;
 
 public class Principal extends ApplicationAdapter {
     private OrthographicCamera camara;
-    private TiledMap mapa;
-    private OrthogonalTiledMapRenderer renderMapa;
-
-    private Texture fondo;
     private SpriteBatch batch;
-
-    private int mapaPixelAncho, mapaPixelAlto;
+    private Mapa mapa;
 
     @Override
     public void create() {
-        fondo = new Texture("fondos/fondo.png");
+        camara = new OrthographicCamera();
         batch = new SpriteBatch();
 
-        // Cargar mapa y obtener dimensiones en píxeles
-        mapa = new TmxMapLoader().load("mapas/mapa.tmx");
-        renderMapa = new OrthogonalTiledMapRenderer(mapa);
-
-        MapProperties props = mapa.getProperties();
-        int tileWidth = props.get("tilewidth", Integer.class);
-        int tileHeight = props.get("tileheight", Integer.class);
-        int mapWidth = props.get("width", Integer.class);
-        int mapHeight = props.get("height", Integer.class);
-
-
-        mapaPixelAncho = tileWidth * mapWidth;
-        mapaPixelAlto = tileHeight * mapHeight;
-
-        // Crear cámara ajustada
-        camara = new OrthographicCamera();
-        ajustarCamara(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        // Calcula el zoom para que el mapa entre en pantalla.
-
-    }
-
-
-    // Calcula cuánto debería hacer zoom la cámara para que el mapa entero entre en pantalla.
-    private void ajustarCamara(int anchoPantalla, int altoPantalla) {
-        float escalaX = (float) anchoPantalla / mapaPixelAncho;
-        float escalaY = (float) altoPantalla / mapaPixelAlto;
-        float zoom = 1f / Math.min(escalaX, escalaY); // zoom para que el mapa entre en pantalla
-
-        camara.setToOrtho(false, anchoPantalla, altoPantalla);
-        camara.zoom = zoom;
-        camara.position.set(mapaPixelAncho / 2f, mapaPixelAlto / 2f, 0);
-        camara.update();
+        // Eleccion del mapa a cargar
+        mapa = new Mapa(MapasDisponibles.MAPA_BOSQUE);
+        mapa.ajustarCamara(camara, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
@@ -66,30 +29,20 @@ public class Principal extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camara.update();
-
-        // Dibujar fondo
         batch.setProjectionMatrix(camara.combined);
-        batch.begin();
-        batch.draw(fondo, camara.position.x - (camara.viewportWidth * camara.zoom) / 2f,
-            camara.position.y - (camara.viewportHeight * camara.zoom) / 2f,
-            camara.viewportWidth * camara.zoom,
-            camara.viewportHeight * camara.zoom);
-        batch.end();
 
-        // Renderizar mapa
-        renderMapa.setView(camara);
-        renderMapa.render();
+        mapa.renderFondo(batch, camara);
+        mapa.renderMapa(camara);
     }
 
     @Override
     public void resize(int width, int height) {
-        ajustarCamara(width, height);
+        mapa.ajustarCamara(camara, width, height);
     }
 
     @Override
     public void dispose() {
-        fondo.dispose();
-        batch.dispose();
         mapa.dispose();
+        batch.dispose();
     }
 }
