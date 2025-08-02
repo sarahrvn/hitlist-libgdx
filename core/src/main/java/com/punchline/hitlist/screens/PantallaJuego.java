ae2e5955a222107058355fb1a422144d3218c58f
Sarah Villarreal Carballo <sarahvc007@gmail.com>
2025-08-02 01:05:36 -0300

    Actualización

--- core/src/main/java/com/punchline/hitlist/screens/PantallaJuego.java
+++ core/src/main/java/com/punchline/hitlist/screens/PantallaJuego.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.punchline.hitlist.elementosJuego.Mapa;
 import com.punchline.hitlist.elementosJuego.MapaDisponible;
 import com.punchline.hitlist.personajes.Personaje;
+import com.punchline.hitlist.personajes.TipoPersonaje;
 
 public class PantallaJuego {
 
     private final Mapa mapa;
+    private Personaje personaje1 = new Personaje(TipoPersonaje.TIPAZO);; //PROVISIONAL. En realidad, la PantallaSeleccion es la encargada de, justamente, indicar el personaje elegido y pasárselo a PantallaJuego.
 
     public PantallaJuego() {
         mapa = new Mapa(MapaDisponible.MAPA_BOSQUE);
@@ -18,10 +20,11 @@ public class PantallaJuego {
         mapa.ajustarCamara(camara, ancho, alto);
     }
 
-    public void render(SpriteBatch batch, OrthographicCamera camara, Personaje personaje1) {
+    public void render(SpriteBatch batch, OrthographicCamera camara) {
         mapa.renderFondo(batch, camara);
         mapa.renderMapa(camara);
         personaje1.dibujar(batch);
+        personaje1.realizarMovimientos();
     }
 
     public void dispose() {
