1add6ec003fbfa554494d19f626a52e7fff147dc
Sarah Villarreal Carballo <sarahvc007@gmail.com>
2025-11-20 16:42:02 -0300

    Actualización

--- core/src/main/java/com/punchline/hitlist/screens/PantallaJuego.java
+++ core/src/main/java/com/punchline/hitlist/screens/PantallaJuego.java
@@ -2,6 +2,7 @@ package com.punchline.hitlist.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,11 +10,12 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.punchline.hitlist.elementosJuego.Mapa;
 import com.punchline.hitlist.elementosJuego.MapaDisponible;
+import com.punchline.hitlist.interacciones.TeclaListener;
 import com.punchline.hitlist.personajes.Personaje;
 import com.punchline.hitlist.personajes.TipoPersonaje;
 import com.punchline.hitlist.elementosJuego.Hud;
 
-public class PantallaJuego {
+public class PantallaJuego implements Screen {
 
     private Mapa mapa;
     private final Personaje PERSONAJE_1;
@@ -23,6 +25,7 @@ public class PantallaJuego {
     private boolean enPausa = false;
     private float tiempoTranscurrido = 0;
     private boolean tiempoCumplido = false;
+    private TeclaListener teclaListener = new TeclaListener();
 
     public PantallaJuego() {
 
@@ -38,6 +41,16 @@ public class PantallaJuego {
         camaraJuego.update();
 
         PERSONAJE_1.setPosition(mapa.getAncho() / 2f, mapa.getAlto() / 2f);
+    }
+
+    @Override
+    public void show(){
+        Gdx.input.setInputProcessor(this.teclaListener);
+    }
+
+    @Override
+    public void render(float delta) {
+
     }
 
     public void render(SpriteBatch batch) {
@@ -64,7 +77,7 @@ public class PantallaJuego {
     public void update(){
         float delta = Gdx.graphics.getDeltaTime();
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+        if (teclaListener.isEscapeJustPressed()) {
             enPausa = !enPausa;
         }
 
@@ -77,7 +90,8 @@ public class PantallaJuego {
                 tiempoCumplido = true;
             }
 
-            //PERSONAJE_1.realizarMovimientos(mapa.getColisiones());
+
+            this.PERSONAJE_1.realizarMovimientos(mapa.getColisiones());
 
 
         }
@@ -95,5 +109,30 @@ public class PantallaJuego {
         HUD.dispose();
     }
 
+
+
+
+
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
 
 }
