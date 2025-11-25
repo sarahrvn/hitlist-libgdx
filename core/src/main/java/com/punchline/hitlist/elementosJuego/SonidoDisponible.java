ef9876c185ce395c26a6237b7d25787a3201a61d
Thiago Ivan Orue Stocco <thiagoivanorue@gmail.com>
2025-11-25 02:52:52 -0300

    Actualizacion

--- core/src/main/java/com/punchline/hitlist/elementosJuego/SonidoDisponible.java
+++ core/src/main/java/com/punchline/hitlist/elementosJuego/SonidoDisponible.java
@@ -4,8 +4,6 @@ public enum SonidoDisponible {
     // Sonidos de personaje
     SALTO("sonidos/salto.wav"),
     CAIDA("sonidos/caida.ogg"),
-    GOLPE("sonidos/golpe.wav"),
-    ESPADA("sonidos/agarrar_espada.mp3"),
     MENU("sonidos/menu_mover.mp3"),
 
     // Música de fondo
